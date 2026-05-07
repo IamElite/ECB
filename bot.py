f7a0548654f888d0480adc6f4675b97ac5342e16
NoobDuru <gokhaledurgesh4@gmail.com>
2026-05-07 15:09:37 +0530

    feat: move config to dedicated config.py module

--- bot.py
+++ bot.py
@@ -14,13 +14,8 @@ from pyrogram import raw
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, BotCommand
 from hypertg import HyperTGDownload, HyperTGUpload
 
-# 👇 Environment variables from Heroku config
-API_ID = int(os.environ.get("API_ID", 14050586))
-API_HASH = os.environ.get("API_HASH", "42a60d9c657b106370c79bb0a8ac560c")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8266949820:AAH5AZ58is4bI06UXmytuaSLIP0mKEnXMa8")
-ADMIN_ID = int(os.environ.get("ADMIN_ID", 7074383232))
-UPSTREAM_REPO = os.environ.get("UPSTREAM_REPO", "https://github.com/IamElite/ECB")
-UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "main")
+# 👇 Environment variables from Heroku config (via config.py)
+from config import API_ID, API_HASH, BOT_TOKEN, ADMIN_ID, UPSTREAM_REPO, UPSTREAM_BRANCH
 
 app = Client("EncodeBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
